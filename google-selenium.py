02450005a112c91f926c443d3801102c55269bdd
wiwich <wiwich0401@gmail.com>
2024-07-19 17:50:15 +0700

    Update google-selenium.py

--- google-selenium.py
+++ google-selenium.py
@@ -6,13 +6,21 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 # set the path to Chrome driver
 CHROMEDRIVER_PATH = r"C:\<path>\chromedriver.exe"
 
-# initial the chrome driver
-options = webdriver.ChromeOptions()
-service = ChromeService(executable_path=CHROMEDRIVER_PATH)
-driver = webdriver.Chrome(service=service, options=options)
+###
+### Function: navigate_chrome
+### navigate to the URL
+###
+def navigate_chrome(url):
+    options = webdriver.ChromeOptions()
+    service = ChromeService(executable_path=CHROMEDRIVER_PATH)
+    driver = webdriver.Chrome(service=service, options=options)
 
-# navigate to the URL 
-driver.get("https://www.google.com")
+    driver.get(url)
 
-# to close the browser when done
-driver.quit()
+    # to close the browser when done
+    # driver.quit()
+
+### Example usage
+if __name__=="main":
+    target_url = "https://www.google.com"
+    navigate_chrome(target_url)
